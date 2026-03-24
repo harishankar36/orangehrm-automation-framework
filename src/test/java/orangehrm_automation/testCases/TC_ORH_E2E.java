2296cb81f96eb8ab1ec66e8c84a79d92a315a694
hari4 <hari46.info@gmail.com>
2026-03-24 14:19:48 -0400

    altered E2E implementing POM

--- src/test/java/orangehrm_automation/testCases/TC_ORH_E2E.java
+++ src/test/java/orangehrm_automation/testCases/TC_ORH_E2E.java
@@ -1,20 +1,30 @@
 package orangehrm_automation.testCases;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import orangehrm_automation.Base.BaseTest;
 import orangehrm_automation.pageObjects.LoginPage;
+import orangehrm_automation.pageObjects.PageObjectManager;
 
 
 public class TC_ORH_E2E extends BaseTest{
 	
 	
-	@Test
-	public void TC_ORH_004()
+	protected PageObjectManager pom;
+	
+	@BeforeMethod
+	public void setupPages()
 	{
-		LoginPage loginPage = new LoginPage(driver);
+		pom = new PageObjectManager(driver);
 		
-		loginPage.login("Admin", "admin123");
+	}
+	
+	@Test
+	public void TC_ORH_chk()
+	{
+
+		pom.getLoginPage().login("Admin", "admin123");
 		
 		//validating whether system is in the dashboard page
 		
